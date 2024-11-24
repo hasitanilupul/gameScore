45022a82050d24b9c91c53a6cfeaf4057c210fae
Hasitha Nilupul <hasitanilupul@gmail.com>
2024-11-24 22:18:41 +0530

    top 10 users

--- src/main/java/com/gamescore/gamescore/controller/usersGamesController.java
+++ src/main/java/com/gamescore/gamescore/controller/usersGamesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gamescore.gamescore.model.UsersGames;
@@ -38,46 +39,47 @@ public class usersGamesController {
 
         List<UsersGames> users = usersGamesService.getAllUsers();
 
-        for(UsersGames i : users) {
+        for (UsersGames i : users) {
             System.out.println("user " + i.getUserId() + " Game " + i.getGameId() + " score " + i.getScore());
         }
 
         List<UsersGames> distinctHighestScores = new ArrayList<>();
-        
+
         distinctHighestScores = users.stream()
-            .collect(Collectors.groupingBy(p -> Pair.of(p.getUserId(), p.getGameId()),
-                Collectors.maxBy(Comparator.comparingDouble(UsersGames::getScore))))
-            .values()
-            .stream()
-            .map(Optional::get)
-            .collect(Collectors.toList());
-        
-        for(UsersGames i : distinctHighestScores) {
-            System.out.println(" filterd user id " + i.getUserId() + " game id " + i.getGameId() +  " score " + i.getScore());
+                .collect(Collectors.groupingBy(p -> Pair.of(p.getUserId(), p.getGameId()),
+                        Collectors.maxBy(Comparator.comparingDouble(UsersGames::getScore))))
+                .values()
+                .stream()
+                .map(Optional::get)
+                .collect(Collectors.toList());
+
+        for (UsersGames i : distinctHighestScores) {
+            System.out.println(
+                    " filterd user id " + i.getUserId() + " game id " + i.getGameId() + " score " + i.getScore());
         }
 
-        // for (UsersGames obj : users) {
-        //     boolean found = false;
-        //     for (UsersGames existing : distinctHighestScores) {
-        //         if (existing.getUserId() == obj.getUserId() && existing.getGameId() == obj.getGameId()) {
-        //             if (obj.getScore() > existing.getScore()) {
-        //                 distinctHighestScores.remove(existing);
-        //                 distinctHighestScores.add(obj);
-        //             }
-        //             found = true;
-        //             break;
-        //         }
-        //     }
-        //     if (!found) {
-        //         distinctHighestScores.add(obj);
-        //     }
-        // }
+        return distinctHighestScores; // server will send the User ID and Game ID, In the front end these ids need to
+                                      // map relavane user name and game name
+    }
 
-        return distinctHighestScores; // server will send the User ID and Game ID, In the front end these ids need to map relavane user name and game name
+    @GetMapping("/top10")
+    public List<UsersGames> gettop10(@RequestParam int gameId) {
+        System.out.println("Get top 10 users");
+
+        List<UsersGames> allRecs = usersGamesService.getAllUsers();
+
+        List<UsersGames> top10t = allRecs.stream().filter(list-> list.getGameId() == gameId)
+                .sorted(Comparator.comparing(UsersGames::getScore).reversed())
+                .limit(10)
+                .collect(Collectors.toList());
+
+        return top10t;
+
     }
 
     @PostMapping
-    public ResponseEntity<UsersGames> createUser(@RequestBody UsersGames usersGames) { // Frontend will send the userId and GameId in the body rather than sending user name and game name
+    public ResponseEntity<UsersGames> createUser(@RequestBody UsersGames usersGames) { // Frontend will send the userId and GameId in the body rather
+                                                                                       // than sending user name and game name
         System.out.println("Save UsersGames");
         try {
             UsersGames savedUsersGames = usersGamesService.savedUsersGames(usersGames);
